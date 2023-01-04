37320e52d72f645b87e19c793ccc8495d7193089
nordicon1 <0w0.ebi.kaitai@gmail.com>
2023-01-05 04:54:38 +0900

    beale.py

--- beale.py
+++ beale.py
@@ -2,20 +2,22 @@ import numpy as np
 import matplotlib.pyplot as plt
 from mpl_toolkits.mplot3d import Axes3D
 
-# name: Ackley function
-# search area: −32.768 <= xi <= 32.768
-# best solution: f(0, 0) = 0
+# name: Beale function
+# search area: -4.5 <= xi <= 4.5
+# best solution: f(3, 0.5) = 0
 
 
 def func(xs: np.ndarray) -> np.float64:
-    n = len(xs)
-    y = 20 - 20*np.exp(-0.2*np.sqrt(1/n*np.sum(xs**2))) + np.e - np.exp(1/n*np.sum(np.cos(2*np.pi*xs)))
+    x1 = xs[0]
+    x2 = xs[1]
+    y = (1.5 - x1 + x1*x2)**2 + (2.25 - x1 + x1*x2**2)**2 + (2.625 - x1 + x1*x2**3)**2
+
     return y
 
 
 if __name__ == "__main__":
-    lower = -32.768
-    upper = 32.768
+    lower = -4.5
+    upper = 4.5
     x0 = np.linspace(lower, upper, 32)
     x1 = np.linspace(lower, upper, 32)
     y = np.array([func(np.array([_x0, _x1])) for _x0 in x0 for _x1 in x1])
